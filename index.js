5bea2eaa9b88b22db727b2927a38c81478403f21
TafLaw <muzerenganit@gmail.com>
2019-12-11 08:15:16 +0200

    all

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
+// const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 var test = "this is a test";
 app.use(express.static('public'));
+// app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get("/", function(req, res){
     res.render('register', {test: test});
 });
+
+app.post('/', function(req, res){
+    var name = req.body.firstname;
+    var surname = req.body.secondname;
+    var email = req.body.email;
+    console.log(name);
+    console.log(surname);
+    console.log(email);
+    res.render('about', {name: name});
+});
+
+/* app.post('/register', function(req, res){
+
+}); */
 
 app.listen(8080);
