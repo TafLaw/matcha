1fb8833bf2abd0432d24bf311f053c6631449ee7
TafLaw <muzerenganit@gmail.com>
2019-12-12 11:05:41 +0200

    all

--- models/database/setup.js
+++ models/database/setup.js
@@ -1,8 +1,20 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:8080/matcha";
+var url = "mongodb://localhost:27017/matcha";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
+});
+
+var ur = "mongodb://localhost:27017/";
+
+MongoClient.connect(ur, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("matcha");
+  dbo.createCollection("users", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-/* router.get('/', function(req, res, next) {
-  console.log("Handled get request on the home page.");
-  res.render('index', { title: 'TAF' });
-});
-
-router.post('/', (req, res, next) => {
-   console.log("Handled a post request from the home page.");
-}); */
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 router.get("/", function(req, res){
   res.render('index');
@@ -19,15 +11,30 @@ router.post('/', function(req, res){
   var name = req.body.firstname;
   var surname = req.body.secondname;
   var email = req.body.email;
+  var pass = req.body.password;
   var sub = req.body.submit
-  if (sub === "Sign Up")
-      res.render('login', {name: "signup"});
+  if (sub === "Sign Up"){
+    var data = { 
+      "name": name,
+      "surname": surname,
+      "email":email, 
+      "password":pass,
+      "notifications": 1
+    }
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("matcha");
+      dbo.collection('users').insertOne(data, function(err, res) {
+        if (err) throw err;
+        console.log("record inserted successfully!");
+        db.close();
+      });      
+  });
+  //redirect
+    res.render('login', {name: "signup"});
+  }
   else
       res.render('login', {name: "login"});
-  console.log(name);
-  console.log(sub);
-  console.log(surname);
-  console.log(email);
 });
 
 module.exports = router;
