f162ed41742443446cdc872a6c0e10fe0d9a229c
mnzolo <mnzolo@student.wethinkcode.co.za>
2020-01-13 08:49:20 +0200

    prof

--- routes/profile.js
+++ routes/profile.js
@@ -13,33 +13,29 @@ router.use(bodyparser.urlencoded({ extended: true }));
 
 router.post("/", function (req, res) {
     console.log(req.body);
-    if (req.body.updateAbout == "updateAbout")
-    {
+    if (req.body.updateAbout == "updateAbout") {
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             var dbo = db.db('matcha');
-            var myobject = {about: req.body.aboutprofile}
+            var myobject = { email: req.session.user.email, about: req.body.aboutprofile }
 
-            dbo.collection("profile").insertOne(myobject, function(err, res)
-            {
-                if(err) throw err;
+            dbo.collection("profiletext").insertOne(myobject, function (err, res) {
+                if (err) throw err;
                 console.log("Inserted about text");
             });
             db.close();
         });
         res.render('profile');
     }
-    else if(req.body.saveAbout == "saveAbout")
-    {
+    else if (req.body.saveAbout == "saveAbout") {
         console.log("updating inputs");
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             var dbo = db.db('matcha');
-            var myobject = {diet: req.body.diet , race: req.body.race, gender: req.body.gender, height: req.body.height}
+            var myobject = { email: req.session.user.email, diet: req.body.diet, race: req.body.race, gender: req.body.gender, height: req.body.height }
 
-            dbo.collection("profile").insertOne(myobject, function(err, res)
-            {
-                if(err) throw err;
+            dbo.collection("profile").insertOne(myobject, function (err, res) {
+                if (err) throw err;
                 console.log("Inserted Select options");
             });
             db.close();
@@ -64,68 +60,78 @@ router.post("/", function (req, res) {
         console.log("Finding your current location");
         res.render('profile');
     }
-    else if (req.body.upload == undefined) {
+    else if (req.body.upload == null) {
         var form = new formidable.IncomingForm();
 
         form.parse(req, function (err, fields, files) {
             var oldpath = files.fileuploaded.path;
-            var newpath = __dirname.replace("routes","public") + "/images/" + files.fileuploaded.name;
-            fs.rename(oldpath, newpath, function(err)
-            {
-                if(err) throw err;
-                console.log(newpath);
+            /* var newpath = __dirname.replace("routes","public") + "/images/" + files.fileuploaded.name; */
+            if (files.fileuploaded.size == 0) {
+                console.log("file field is empty");
+                res.redirect('http://localhost:8080/profile');
+            }
+            else {
+                fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw err;
+                    console.log(newpath);
+                });
                 console.log("file has been moved");
             }
-            );
             res.end();
         });
     }
 }
 );
 
- /* router.post("/uploadprofile", function (req, res) {
-    var form = new formidable.IncomingForm();
+/* router.post("/uploadprofile", function (req, res) {
+   var form = new formidable.IncomingForm();
 
-    form.parse(req, function (err, fields, files) {
-        var oldpath = files.fileuploaded.path;
-        var newpath = __dirname.replace("routes", "public") + "/images/" + files.fileuploaded.name;
-        fs.rename(oldpath, newpath, function (err) {
-            if (err) throw err;
-            console.log(newpath);
-            console.log("file has been moved");
-        }
-        );
-        res.end();
-    }); 
+   form.parse(req, function (err, fields, files) {
+       var oldpath = files.fileuploaded.path;
+       var newpath = __dirname.replace("routes", "public") + "/images/" + files.fileuploaded.name;
+       fs.rename(oldpath, newpath, function (err) {
+           if (err) throw err;
+           console.log(newpath);
+           console.log("file has been moved");
+       }
+       );
+       res.end();
+   }); 
 });*/
 
 router.get("/", function (req, res) {
+    var username1 = null;
+    var texta = null;
+    var age = null;
+    var diet = null;
+    var race = null;
+    var gender = null;
+    var height = null;
+
     MongoClient.connect(url, function (err, db) {
-        if (err) throw err; 
+        if (err) throw err;
         //console.log("connecting for profile");
         var dbo = db.db('matcha');
-        dbo.createCollection("profile", function(err,res)
-        {
-            if(err) throw err;
-            console.log("profile colection created");
-        });
-        
-        var query = { name: "Mthokozisi" };
-        
+
+        var query = { email: req.session.user.email };
+
         dbo.collection("users").find(query).toArray(function (err, result) {
             if (err) throw err;
-            result.forEach(function (userlog) {
-                username = userlog.name;
-                age = Number(17);
+            //console.log(result);
+            console.log("Myname");
+            result.forEach(function (user) {
+                username1 = user.name;
+                //age = Number(17);
             })
+            console.log(username1);
         }
         );
-        
+
         //the about text reqiures sessions
 
         /* var q = {_id: "5dfa32768d809f7317c01cf8"};
-                    {about:}
-
+        {about:}
+        
         dbo.collection("profile").find(q).toArray(function (err, result1) {
             if (err) throw err;
             result1.forEach(function (about) {
@@ -135,9 +141,9 @@ router.get("/", function (req, res) {
             console.log(result1);
         }
         );  */
-        
-    var query1 = {height: "1"}
-        
+
+        var query1 = { email: req.session.user.email }
+
         dbo.collection("profile").find(query1).toArray(function (err, result2) {
             if (err) throw err;
             result2.forEach(function (userabout) {
@@ -146,23 +152,64 @@ router.get("/", function (req, res) {
                 gender = userabout.gender;
                 height = userabout.height;
             });
-            console.log(result2);
+            //console.log(result2);
         }
         );
+
+        var query2 = { email: req.session.user.email }
+
+        dbo.collection("profiletext").find(query2).toArray(function (err, result3) {
+            if (err) throw err;
+            //console.log("result3"); 
+            //console.log(result3);
+            result3.forEach(function (textb) {
+                texta = textb.about;
+            });
+            //console.log(result3);
+        }
+        );
+
         db.close();
-    }
-    );
+    });
 
-    var text = "Thank you God for the love and Grace...";
-
-   /*  var geolocation = require('geolocation')
- 
-geolocation.getCurrentPosition(function (err, position) {
-  if (err) throw err
-  console.log(position)
-}) */
+    /*  var geolocation = require('geolocation')
     
-    res.render('profile' , { username: username, age: age, text: text , diet: diet, race: race, gender: gender, height: height});
+    geolocation.getCurrentPosition(function (err, position) {
+        if (err) throw err
+        console.log(position)
+    }) */
+    if (texta == null) {
+        console.log("whats wrong");
+        console.log(username1);
+    }
+    //console.log(diet);
+
+    /* if (username1 == null)
+    {
+        username1 = '';
+    }
+    else if(diet == null)
+    {
+        diet = '';
+    }
+    else if (race == null)
+    {
+        race = '';
+    }
+    else if(gender == null)
+    {
+        gender = '';
+    }
+    else if (height == null)
+    {
+        height = '';
+    }
+    else if(text == null)
+    {
+        text = '';
+    }*/
+    //  username1 = "Mthokozisi";
+    res.render('profile', { username1: username1 /*age: age  text: texta */ /* , diet: diet, race: race, gender: gender, height: height */ });
 });
 
 /*
