26e5143572f119f53a1860e5739d4ac7ffb4dd02
TafLaw <muzerenganit@gmail.com>
2020-01-29 15:43:02 +0200

    javascripts

---
+++ public/javascripts/filterpop.js
@@ -0,0 +1,8 @@
+function clicked_show(){
+    document.getElementById("filt").style.display = "block"
+    document.getElementById("ms").style.display = "none"
+}
+
+function clicked_hide(){
+    document.getElementById('filt').style.display = "none";
+}
